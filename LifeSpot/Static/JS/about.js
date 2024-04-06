c34895cf8b39fb238c17ccd8150ff613b8fd047a
Maksim <craash00077@gmail.com>
2024-04-06 11:30:21 +0300

    29.7

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,65 @@
+﻿
+
+
+const rateButton = document.getElementById('rateButton');
+
+
+
+function rateProject() {
+
+
+    let review = {
+        name: '',
+        comment: '',
+        date: ''
+    }
+
+
+    review["name"] = prompt("Напишите свое имя")
+    if (review["name"] == null) {
+        return
+    }
+
+    review["comment"] = prompt("Напишите свой отзыв")
+
+    if (review["comment"] == null) {
+        return
+    }
+
+    review["date"] = new Date().toLocaleString()
+
+   
+    writeOnPage(review)
+
+    console.log(review["name"])
+    console.log(review["comment"])
+    
+}
+
+const writeOnPage = review => {
+
+
+    document.getElementsByClassName('rates')[0].innerHTML += `
+
+    
+        <div class="name">
+            ${review["name"]}
+        </div>
+        <div class="comment">
+            ${review["comment"]}
+        </div>
+        <div class="date">
+            ${review["date"]}
+        </div>
+    
+    
+    `
+
+}
+
+
+
+
+
+
+rateButton.addEventListener('click', rateProject);
---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,67 @@
+﻿
+let session = new Map();
+
+
+function checkAge() {
+
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+const subsctiptionAlert = function() {
+    alert("Подпишитесь, на нас, пожалуйста")
+}
+
+
+
+function handleSession() {
+
+    session.set("startDate", new Date().toLocaleString())
+    session.set("userAgent", window.navigator.userAgent)
+
+
+    for (let result of session) {
+        console.log(result)
+    }
+
+    const intervalId = setInterval(subsctiptionAlert, 5000)
+
+    const timeoutId = setTimeout(function () {
+        clearInterval(intervalId)
+    }, 5000)
+
+}
+
+let sessionLog = function logSession() {
+    for (let result of session) {
+        console.log(result)
+    }
+}
+
+
+const inputParseFunction = function () {
+    return document.getElementsByTagName('input')[0].value.toLowerCase();
+}
+
+function filterContent() {
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++) {
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        if (!videoText.toLowerCase().includes(inputParseFunction() /* Захват переменной теперь происходит с помощью замыкания */.toLowerCase())) {
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
